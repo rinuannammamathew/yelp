d02c2282673372e1028c6346ef7fcbefce71f095
Rinu Mathew <rinuannammamathew@gmail.com>
2019-06-25 20:48:42 +0000

    Add a good message describing what you actually changed since your last commit in here

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,9 @@
 class PlacesController < ApplicationController
   def index
     @places = Place.all
+    @places = Place.order("name").page(params[:page]).per_page(1)
+  end
+  def new
+    @place = Place.new
   end
 end
