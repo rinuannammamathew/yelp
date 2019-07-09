1a11d0deb8d01d28089fdafcd944510dd1655624
Rinu Mathew <rinuannammamathew@gmail.com>
2019-07-09 03:13:12 +0000

    Add a good message describing what you actually changed since your last commit in here

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,10 @@
+class NotificationMailer < ApplicationMailer
+  default from: "no-reply@yelpapp.com"
+
+  def comment_added
+    def comment_added
+    mail(to: "inu@gmail.com",
+         subject: "A comment has been added to your place")
+    end
+  end
+end
